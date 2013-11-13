afc62795ca5ad5d08768ff85c8b3fc4c0f91b819
olivier <olivier.duhart@gmail.com>
2013-11-13 07:11:40 +0100

    compile for all platforms

--- GruntFile.js
+++ GruntFile.js
@@ -9,10 +9,10 @@ module.exports = function(grunt) {
     options: {
         version: '0.8.0',
         build_dir: './webkitbuilds', // Where the build version of my node-webkit app is saved
-        mac: false, // We want to build it for mac
+        mac: true, // We want to build it for mac
         win: true, // We want to build it for win
-        linux32: false, // We don't need linux32
-        linux64: false // We don't need linux64
+        linux32: true, // We don't need linux32
+        linux64: true // We don't need linux64
     },
     src: ['./**/*'] // Your node-wekit app
   },
